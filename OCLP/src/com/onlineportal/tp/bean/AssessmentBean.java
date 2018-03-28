919dc01acb94ca079984dcba388df7ae7703e617
NikhileshAS <32016285+NikhileshAS@users.noreply.github.com>
2018-03-28 11:29:37 +0530

    Assessment

--- OCLP/src/com/onlineportal/tp/bean/AssessmentBean.java
+++ OCLP/src/com/onlineportal/tp/bean/AssessmentBean.java
@@ -1,3 +1,4 @@
+<<<<<<< Upstream, based on branch 'master' of https://github.com/NikhileshAS/OCLP.git
 package com.onlineportal.tp.bean;
 import java.util.Date;
 
@@ -47,3 +48,54 @@ public class AssessmentBean {
 	
 
 }
+=======
+package com.onlineportal.tp.bean;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table( name = "Assessment")
+public class AssessmentBean {
+
+	int testCount = 0;
+	@Id private String testId;
+	private String courseId;
+	private String facultyId;
+	private Question question[];
+	
+	
+		
+	public Question[] getQuestion() {
+		return question;
+	}
+	public void setQuestion(Question question[]) {
+		this.question = question;
+	}
+	public String getTestId() {
+		return testId;
+	}
+	public void setTestId() {
+		this.testId = ++testCount+"";
+	}
+
+	public String getCourseId() {
+		return courseId;
+	}
+	public void setCourseId(String courseId) {
+		this.courseId = courseId;
+	}
+	public String getFacultyId() {
+		return facultyId;
+	}
+	public void setFacultyId(String facultyId) {
+		this.facultyId = facultyId;
+	}
+	
+	
+
+}
+>>>>>>> e5f0b52 Assessment
