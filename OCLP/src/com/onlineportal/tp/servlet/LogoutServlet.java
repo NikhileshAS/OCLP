e394417f36f7e73de89d968959769f605a44163f
NikhileshAS <32016285+NikhileshAS@users.noreply.github.com>
2018-03-28 10:31:55 +0530

    Add files via upload

---
+++ OCLP/src/com/onlineportal/tp/servlet/LogoutServlet.java
@@ -0,0 +1,27 @@
+package com.onlineportal.tp.servlet;
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/Faculty/LogoutServlet")
+
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		session.invalidate();
+		response.sendRedirect("../Home/index.jsp");
+	}
+
+	
+
+}
